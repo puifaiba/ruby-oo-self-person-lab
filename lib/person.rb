840a5bf45ec13e34c9018d94438518d71ff5989b
puifaiba <puifai.ba@gmail.com>
2021-01-14 16:13:34 -0500

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -48,19 +48,35 @@ class Person
     end
 
     def take_bath
-        @hygiene += 4
+        self.hygiene += 4
         return "♪ Rub-a-dub just relaxing in the tub ♫"
     end
 
     def work_out
-        @happiness += 2
-        @hygiene += 3
+        self.happiness += 2
+        self.hygiene -= 3
         return  "♪ another one bites the dust ♫"      
     end
+
+    def call_friend(friend)
+        friend.happiness += 3
+        self.happiness += 3
+        return "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(person, topic)
+        if topic == "politics"
+            person.happiness -= 2
+            self.happiness -= 2
+            return "blah blah partisan blah lobbyist"
+        elsif topic == "weather"
+            person.happiness += 1
+            self.happiness += 1
+            return "blah blah sun blah rain"
+        else 
+            return "blah blah blah blah blah"
+        end
+    end
+            
 end
 
-
-
-
-# Pearce = Person.new("Nick")
-# Pearce.name("Liz")
